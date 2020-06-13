96d9cd5ca96076c6bed3383147159dc44c98a6f5
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2020-06-13 17:56:50 +0900

    modify uri

--- api/handler.go
+++ api/handler.go
@@ -2,19 +2,29 @@ package api
 
 import (
 	"encoding/json"
-	"github.com/gorilla/mux"
+	"fmt"
+	"log"
 	"net/http"
 )
 
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", " text/plain")
+	n, err := fmt.Fprintf(w, "WasedaTime is Back!")
+	if err != nil {
+		log.Println(n, err)
+	}
+}
+
 func courseCommentHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", " application/json")
-	courseId := mux.Vars(r)["course_id"]
+	params := r.URL.Query()
+	courseId := params.Get("course_id")
 	resp := findCourseCommentByID(courseId)
 	if resp.CourseId == "" {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
-	err := json.NewEncoder(w).Encode(&resp)
+	err := json.NewEncoder(w).Encode(resp)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
--- api/http.go
+++ api/http.go
@@ -8,7 +8,8 @@ import (
 
 func Start() {
 	r := mux.NewRouter()
-	r.HandleFunc("/comment/{course_id}", courseCommentHandler).Methods(http.MethodGet)
+	r.HandleFunc("/", helloWorldHandler).Methods(http.MethodGet)
+	r.HandleFunc("/comments", courseCommentHandler).Methods(http.MethodGet)
 	log.Println("Started api service at port 8080")
 	err := http.ListenAndServe(":8080", r)
 	if err != nil {
