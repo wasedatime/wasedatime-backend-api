858204099c4946e6e502bbfa356353578203e271
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2020-06-17 12:09:25 +0900

    modify field name

--- api/handler.go
+++ api/handler.go
@@ -18,7 +18,7 @@ func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 func courseCommentHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", " application/json")
 	params := r.URL.Query()
-	courseId := params.Get("course_id")
+	courseId := params.Get("course_code")
 	resp := findCourseCommentByID(courseId)
 	err := json.NewEncoder(w).Encode(resp)
 	if err != nil {
--- api/view.go
+++ api/view.go
@@ -4,12 +4,12 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type CourseComment struct {
 	Id            primitive.ObjectID `bson:"_id" json:"-"`
-	CourseId      string             `bson:"course_id" json:"course_id"`
+	CourseCode    string             `bson:"course_code" json:"course_code"`
 	Title         string             `json:"title"`
 	Instructor    string             `json:"instructor"`
 	Satisfaction  int8               `json:"satisfaction"`
 	Difficulty    int8               `json:"difficulty"`
 	Benefit       int8               `json:"benefit"`
-	CommentedYear int16              `json:"commented_year"`
+	CommentedYear int16              `bson:"commented_year" json:"commented_year"`
 	Comment       string             `json:"comment"`
 }
