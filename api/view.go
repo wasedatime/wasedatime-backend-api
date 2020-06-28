fc130defedf21c086f9c9c28804cf6107b808bf9
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2020-06-28 14:30:00 +0900

    update

--- api/view.go
+++ api/view.go
@@ -3,13 +3,14 @@ package api
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type CourseComment struct {
-	Id            primitive.ObjectID `bson:"_id" json:"-"`
+	ID            primitive.ObjectID `bson:"_id" json:"-"`
+	CourseKey     string             `bson:"course_key" json:"course_key"`
 	CourseCode    string             `bson:"course_code" json:"course_code"`
 	Title         string             `json:"title"`
 	Instructor    string             `json:"instructor"`
 	Satisfaction  int8               `json:"satisfaction"`
 	Difficulty    int8               `json:"difficulty"`
 	Benefit       int8               `json:"benefit"`
-	CommentedYear int16              `bson:"commented_year" json:"commented_year"`
+	CommentedDate int16              `bson:"commented_date" json:"commented_date"`
 	Comment       string             `json:"comment"`
 }
