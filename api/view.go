f1d41d01e83fb8752cd0d469daeba00180e68396
OscarWang114 <chihhaowang.oscar@gmail.com>
2020-08-20 22:45:50 +0800

    view.go: Replace tabs with spaces

--- api/view.go
+++ api/view.go
@@ -10,16 +10,16 @@ type CourseEval struct {
 	CourseKey     	string             `bson:"course_key" json:"course_key"`
 	CourseCode    	string             `bson:"course_code" json:"course_code"`
 	Title         	string             `json:"title"`
-	TitleJp		  	string			   `json:"title_jp"`
+	TitleJp         string             `json:"title_jp"`
 	Instructor    	string             `json:"instructor"`
 	InstructorJp  	string             `json:"instructor_jp"`
 	Satisfaction  	int8               `json:"satisfaction"`
 	Difficulty    	int8               `json:"difficulty"`
 	Benefit       	int8               `json:"benefit"`
-	CommentSrcLng 	int8 			   `json:"comment_src_lng"`
+	CommentSrcLng   int8               `json:"comment_src_lng"`
 	CommentZh       string             `json:"comment_zh"`
-	CommentEn	    string			   `json:"comment_en"`
-	CommentJp		string			   `json:"comment_jp"`
-	Year		    int8 			   `json:"year"`
-	CommentedDate 	time.Time          `bson:"commented_date" json:"commented_date"`
+	CommentEn       string             `json:"comment_en"`
+	CommentJp       string			   `json:"comment_jp"`
+	Year            int8               `json:"year"`
+	CommentedDate   time.Time          `bson:"commented_date" json:"commented_date"`
 }
