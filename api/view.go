00c78ef5dc5673ac86d6bf19b0f8d7efcb173924
OscarWang114 <chihhaowang.oscar@gmail.com>
2020-08-20 22:40:23 +0800

    view.go: conform to new schema

--- api/view.go
+++ api/view.go
@@ -6,14 +6,20 @@ import (
 )
 
 type CourseEval struct {
-	Id            primitive.ObjectID `bson:"_id" json:"-"`
-	CourseKey     string             `bson:"course_key" json:"course_key"`
-	CourseCode    string             `bson:"course_code" json:"course_code"`
-	Title         string             `json:"title"`
-	Instructor    string             `json:"instructor"`
-	Satisfaction  int8               `json:"satisfaction"`
-	Difficulty    int8               `json:"difficulty"`
-	Benefit       int8               `json:"benefit"`
-	Comment       string             `json:"comment"`
-	CommentedDate time.Time          `bson:"commented_date" json:"commented_date"`
+	Id            	primitive.ObjectID `bson:"_id" json:"-"`
+	CourseKey     	string             `bson:"course_key" json:"course_key"`
+	CourseCode    	string             `bson:"course_code" json:"course_code"`
+	Title         	string             `json:"title"`
+	TitleJp		  	string			   `json:"title_jp"`
+	Instructor    	string             `json:"instructor"`
+	InstructorJp  	string             `json:"instructor_jp"`
+	Satisfaction  	int8               `json:"satisfaction"`
+	Difficulty    	int8               `json:"difficulty"`
+	Benefit       	int8               `json:"benefit"`
+	CommentSrcLng 	int8 			   `json:"comment_src_lng"`
+	CommentZh       string             `json:"comment_zh"`
+	CommentEn	    string			   `json:"comment_en"`
+	CommentJp		string			   `json:"comment_jp"`
+	Year		    int8 			   `json:"year"`
+	CommentedDate 	time.Time          `bson:"commented_date" json:"commented_date"`
 }
