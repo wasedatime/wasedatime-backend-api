5da233452cf9be4af17107b5503365deb5be86c1
OscarWang114 <chihhaowang.oscar@gmail.com>
2020-08-25 20:31:47 +0800

    deploy.sh: Add deployment to master server

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -x
 
-if [ $TRAVIS_BRANCH == 'staging' ] ; then
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+    git checkout master
+    git remote add deploy 'deploy@159.89.210.122:wasedatime-backend.git'
+    git add app/app
+    git add deploy_rsa.enc
+    git status
+    git commit -m "Deploy from Travis CI"
+    git status
+    git push deploy staging --force
+elif [ $TRAVIS_BRANCH == 'staging' ] ; then
     git checkout staging
     git remote add deploy 'deploy@139.59.216.161:wasedatime-backend.git'
     git add app/app
