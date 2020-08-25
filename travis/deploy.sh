1a6497ebb5e3be490deac93adaec760e71aa17a2
OscarWang114 <chihhaowang.oscar@gmail.com>
2020-08-25 20:34:25 +0800

    deploy.sh: Fix typo

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -9,7 +9,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
     git status
     git commit -m "Deploy from Travis CI"
     git status
-    git push deploy staging --force
+    git push deploy master --force
 elif [ $TRAVIS_BRANCH == 'staging' ] ; then
     git checkout staging
     git remote add deploy 'deploy@139.59.216.161:wasedatime-backend.git'
@@ -20,5 +20,5 @@ elif [ $TRAVIS_BRANCH == 'staging' ] ; then
     git status
     git push deploy staging --force
 else
-    echo "Not deploying, since this branch isn't staging."
+    echo "Not deploying, since this branch isn't staging nor master."
 fi
